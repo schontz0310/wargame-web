2f62934238ebad1052810a08e42f541cb2fa549a
Usuario <usuario@Usuarios-MacBook-Pro.local>
2026-06-20 21:32:58 -0300

    feat: add Clan Wolf faction support

--- src/components/CardDesktopMercenaryContract.tsx
+++ src/components/CardDesktopMercenaryContract.tsx
@@ -174,6 +174,7 @@ export default function CardDesktop({ selectedCard, isFlipped, getFactionLogo }:
                         { name: "two chevrons", file: "two-chevrons-black.png" },
                         { name: "one chevron", file: "one-chevron-black.png" },
                         { name: "ballistic", file: "balistic.png" },
+                        { name: "Clan Wolf", file: "clan-wolf-black.png" },
                         { name: "VTOL", file: "vtol.png" },
                         { name: "vtol", file: "vtol.png" },
                       ];
